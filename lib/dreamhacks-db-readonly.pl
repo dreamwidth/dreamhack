5135c1f5bb8f2e52255be0e00496b139a6615619
Server Admin <root@hack.dreamwidth.net>
2013-04-29 19:05:39 +0000

    Reorganisation and general update, part 1

---
+++ lib/dreamhacks-db-readonly.pl
@@ -0,0 +1,26 @@
+# database variables for Dreamhacks script
+use DBI;
+
+# include the root settings
+require '/dreamhack/local/helpers/settings.pl';
+my $settings = dh_settings('nonroot');
+
+my $dbserver = "localhost";
+my $dbusername = $settings->{'dh_db_readonly_username'};
+my $dbpassword = $settings->{'dh_db_readonly_password'};
+my $dbdatabase = "dreamhacks";
+
+$db = "DBI:mysql:$dbdatabase:$dbserver";
+$dbh = DBI->connect($db, $dbusername, $dbpassword);
+
+if ($dbh eq undef) {
+  print "<p>Sorry, I couldn't connect to the database for some reason:</p>\n";
+  print "<blockquote><i>" . DBI->errstr . "</i></blockquote>";
+  exit(0);
+}
+
+# other options
+$dbh->{'mysql_enable_utf8'} = 1;
+$dbh->do("SET NAMES utf8");
+
+return 1;
